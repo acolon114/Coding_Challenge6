ecc9e801ec386b793103a8a732b45688b4dc0e81
acolon114 <acolon114@usf.edu>
2024-09-23 02:02:04 +0000

    Create displayEmployeeShifts function

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -5,3 +5,16 @@ const employees = [ // Main employee array
     { name: 'David', shifts: [{ day: 'Monday', hours: 8 }]},
     { name: 'Emily', shifts: [{ day: 'Friday', hours: 8 }]}
 ];
+
+// Task 2: Create a Function to Display Employee Shift Details
+function displayEmployeeShifts(employeeObject) {
+    console.log("Employee: " + employeeObject.name) //Outputs: Employee: (and employee name)
+    employeeObject.shifts.forEach(element => {
+        console.log("   Works " + element['day'] + " for " + element['hours'] + " hours."); // Outputs: Day worked and number of hours
+    });
+ }
+// Each line below runs though the function above
+displayEmployeeShifts(employees[0]);
+displayEmployeeShifts(employees[1]);
+displayEmployeeShifts(employees[2]);
+displayEmployeeShifts(employees[3]);
